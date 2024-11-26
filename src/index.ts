239d04ffbe6f6142a4ef4e7cea5bee6373b3b994
freemode <freemode1614@outlook.com>
2024-11-26 22:39:40 +0800

    chore: update name

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-export const a = "a";
-export const b = "b";
+
+/**
+ * function
+ */
+// eslint-disable-next-line unicorn/prevent-abbreviations
+export function func() {
+  return ""
+}
+
+/**
+ * enum
+ */
+export function enumerate() {
+  return ""
+}
+
+/**
+ * interface
+ */
+export function inter() {
+  return ""
+}
+
+/**
+ * type
+ */
+export function t() {
+  return ""
+}
