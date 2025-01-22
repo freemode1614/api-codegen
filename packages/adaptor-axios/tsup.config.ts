99d3ddc641a2795861103b792a07a9dfe5e7a111
wp.l <wenpeng.lei@ncsi.com.cn>
2025-01-22 17:42:36 +0800

    add config file

---
+++ packages/adaptor-axios/tsup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig([
+  {
+    entry: ["./src/index.ts"],
+    outDir: "./npm",
+    dts: true,
+    format: ["esm", "cjs"],
+    sourcemap: true,
+    clean: process.env.NODE_ENV === "production",
+    treeshake: true,
+    shims: true,
+  },
+  {
+    entry: ["./src/cli.ts"],
+    outDir: "./npm",
+    dts: true,
+    format: ["esm", "cjs"],
+    sourcemap: true,
+    clean: process.env.NODE_ENV === "production",
+    treeshake: true,
+    shims: true,
+  },
+]);
