de75d54e8fb3199eed8b9dad020e3e124921a7bc
freemode <freemode1614@outlook.com>
2025-02-07 22:01:07 +0800

    chore: add vite plugin package

--- packages/postman/__tests__/index.test.ts
+++
@@ -1,8 +0,0 @@
-import add from "../src";
-import { describe, expect, it } from "vitest";
-
-describe("Simple test example", () => {
-  it("one plus one should equals to 2", () => {
-    expect(add(1, 1)).toStrictEqual(2);
-  });
-});
--- packages/postman/src/index.ts
+++
@@ -1,3 +0,0 @@
-export default function Add(a: number, b: number) {
-  return a + b;
-}
