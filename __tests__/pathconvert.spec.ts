12d3ed5156fa538a7cbb9154c7255b41f5f782da
freemode <freemode1614@outlook.com>
2025-02-10 22:11:02 +0800

    rename docs folder add vite-plugin example

--- __tests__/pathconvert.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "vitest";
-
-import pathConvert from "@/utils/pathConvert";
-
-test("pathConvert", () => {
-  expect(pathConvert("/a/{b}/{c}")).toStrictEqual("/a/${b}/${c}");
-  expect(pathConvert("/a/{b}/{-}")).toStrictEqual("/a/${b}/${-}");
-  expect(pathConvert("/a/{b}/{0}")).toStrictEqual("/a/${b}/${0}");
-});
