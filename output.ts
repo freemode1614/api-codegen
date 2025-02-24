54cabd0a1c3430b866595563461550c56019d162
wp.l <wenpeng.lei@ncsi.com.cn>
2025-02-24 17:33:29 +0800

    update ignore

--- output.ts
+++
@@ -1,9 +0,0 @@
-export type Node = {
-  children: Node[];
-};
-export async function postUsingPost(req: { body?: unknown }) {
-  return fetch(`/post`, {
-    method: "POST",
-    body: JSON.stringify(req),
-  }).then(async (response) => (await response.json()) as unknown);
-}
